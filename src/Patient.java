5982560a8d58473535d7bc5f88add25cb3dc1065
Matagaro <nyahumphrey@gmail.com>
2024-03-18 07:20:27 +0300

    created an empty constructor

--- src/Patient.java
+++ src/Patient.java
@@ -1,6 +1,6 @@
 
-class InsufficientAgeException extends Exception {
-    public InsufficientAgeException(String message) {
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
         super(message);
     }
 }
@@ -23,10 +23,10 @@ class Patient {
             if (age >= 0 && age <= 130) {
                 System.out.println("The age that has been entered is valid: " + age + "years");
             } else {
-                throw new InsufficientAgeException("You have entered an age value of: " + age + " years .This is invalid as valid age is " +
+                throw new InvalidAgeException("You have entered an age value of: " + age + " years .This is invalid as valid age is " +
                         "between 0 and 130 years");
             }
-        } catch (InsufficientAgeException e) {
+        } catch (InvalidAgeException e) {
             System.out.println("Age entered is invalid: " + e.getMessage());
         }
 
